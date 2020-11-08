6e4d0bd5956aa696af7f479ebc737ac3207446b3
Toasa <atsushi.tohyama.160.333@gmail.com>
2020-11-08 19:12:11 +0900

    Add cells.

--- main.go
+++ main.go
@@ -9,24 +9,55 @@ const (
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
     CELL_LEN = 20
-    FIELD_WIDTH = CELL_LEN * 10
-    FIELD_HEIGHT = CELL_LEN * 20
+    FIELD_WIDTH = 10
+    FIELD_HEIGHT = 20
 )
 
+type Color uint32
 const (
-    colorField uint32 = 0x00BBBBBB
-	colorI uint32 = 0x00E5282E
-	colorO uint32 = 0x00274696
-	colorS uint32 = 0x00EF7E18
-	colorZ uint32 = 0x002CB099
-	colorJ uint32 = 0x00F8D517
-	colorL uint32 = 0x00DF2384
-	colorT uint32 = 0x005CAD2C
+    colorWindow Color = 0x00000000
+    colorVOID Color = 0x00BBBBBB
+	colorI Color = 0x00E5282E
+	colorO Color = 0x00274696
+	colorS Color = 0x00EF7E18
+	colorZ Color = 0x002CB099
+	colorJ Color = 0x00F8D517
+	colorL Color = 0x00DF2384
+	colorT Color = 0x005CAD2C
 )
 
 type Window struct {
     window *sdl.Window
+    field *Field
+}
+
+type State uint8
+const (
+    VOID State = iota
+    Filled
+    Falling
+)
+
+type Form uint8
+const (
+	I Form = iota
+	O
+	S
+	Z
+	J
+	L
+	T
+)
+
+type Cell struct {
+    state State
+    form Form
+    rect sdl.Rect
+}
+
+type Field struct {
     surface *sdl.Surface
+    board [FIELD_HEIGHT][FIELD_WIDTH]*Cell
 }
 
 func newWindow() (*Window, error) {
@@ -42,23 +73,99 @@ func newWindow() (*Window, error) {
         return nil, err
     }
 
-	surface, err := window.GetSurface()
+    field, err := newField(window)
     if err != nil {
         return nil, err
     }
 
     w := &Window{
         window: window,
-        surface: surface,
+        field: field,
     }
 
     return w, nil
 }
 
+func newField(window *sdl.Window) (*Field, error) {
+    var board [FIELD_HEIGHT][FIELD_WIDTH]*Cell
+    for h := 0; h < FIELD_HEIGHT; h++ {
+        for w := 0; w < FIELD_WIDTH; w++ {
+            board[h][w] = newCell(VOID)
+        }
+    }
+
+	surface, err := window.GetSurface()
+    if err != nil {
+        return nil, err
+    }
+
+    f := &Field{
+        board: board,
+        surface: surface,
+    }
+
+    return f, nil
+}
+
+func newCell(state State) *Cell {
+    return &Cell{ state: state }
+}
+
 func (w *Window) initialize() {
-	field := sdl.Rect{0, 0, FIELD_WIDTH, FIELD_HEIGHT}
-	w.surface.FillRect(&field, colorField)
-	w.window.UpdateSurface()
+    w.field.draw()
+	w.update()
+}
+
+func (w *Window) update() {
+    w.window.UpdateSurface()
+}
+
+func (f *Field) draw() {
+    for h := 0; h < FIELD_HEIGHT; h++ {
+        for w := 0; w < FIELD_WIDTH; w++ {
+            cell := f.board[h][w]
+            cell.draw(h, w, f.surface)
+        }
+    }
+}
+
+func (c *Cell) draw(h, w int, surface *sdl.Surface) {
+    rect := c.getRect(h, w)
+    surface.FillRect(&rect, uint32(c.color()))
+}
+
+func (c *Cell) color() Color {
+    if c.state == VOID {
+        return colorVOID
+    }
+
+    switch c.form {
+    case I:
+        return colorI
+    case O:
+        return colorO
+    case S:
+        return colorS
+    case Z:
+        return colorZ
+    case J:
+        return colorJ
+    case L:
+        return colorL
+    case T:
+        return colorT
+    }
+
+    panic("cell color cannot determined")
+}
+
+func (c *Cell) getRect(h, w int) sdl.Rect{
+    return sdl.Rect {
+        int32(w * CELL_LEN),
+        int32(h * CELL_LEN),
+        CELL_LEN,
+        CELL_LEN,
+    }
 }
 
 func main() {
@@ -72,6 +179,11 @@ func main() {
     }
 
     w.initialize()
+
+    c := newCell(Falling)
+    c.form = I
+    c.draw(2, 3, w.field.surface)
+    w.update()
 
 	running := true
 	for running {
